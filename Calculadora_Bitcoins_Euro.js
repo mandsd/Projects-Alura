b189ffbf8d71ef32313641ca482fef61dcb7e0c1
mandsd <105010000+mandsd@users.noreply.github.com>
2023-01-31 17:22:21 -0300

    Update and rename Calculadora_Bitcoins_Euro to Calculadora_Bitcoins_Euro.js

---
+++ Calculadora_Bitcoins_Euro.js
@@ -0,0 +1,18 @@
+// Abaixo está um projeto que converte Bitcoins para Euro em JavaScript.
+
+var cotacaoDaBitcoin = 21311.89;
+var valorEmEuro;
+
+alert("Seja bem vindo!");
+nome = prompt("Qual o seu nome? ");
+valorEmBitcoin = prompt(
+  "Olá, " + nome + ". Digite um valor em Bitcoin para ser convertido a Euro: "
+);
+
+valorEmEuro = valorEmBitcoin * cotacaoDaBitcoin;
+valorEmEuro = valorEmEuro.toFixed(2);
+
+alert("O valor de ₿ " + valorEmBitcoin + " em Euro é € " + valorEmEuro);
+
+
+// link com html e css colocados https://codepen.io/mandsdahm/full/yLqRoJb
