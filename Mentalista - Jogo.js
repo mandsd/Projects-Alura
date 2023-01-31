25e8cd3d3f41a02e03dba70b137789928321ac62
mandsd <105010000+mandsd@users.noreply.github.com>
2023-01-31 18:53:31 -0300

    Update Mentalista - Jogo.js

--- Mentalista - Jogo.js
+++ Mentalista - Jogo.js
@@ -1,15 +1,16 @@
 // Abaixo está a parte em JavaScript do jogo Mentalista
 // O jogo escolhe um número entre 0 e 1000 e o objetivo é descobrir este número
 // Ao longo dos chutes o programa dirá se o seu chute é maior ou menor do que o número secreto
-// Ao acertar o número secreto será dito depois de  qwuantos chutes você acertou
+// Ao acertar o número secreto será dito depois de quantos chutes você acertou
+//Caso você chegue 15 chutes será mostrada uma mensagem dizendo que você perdeu.
 
-
-var NumeroSecreto = parseInt(Math.random() * 1001);
+var NumeroSecreto = parseInt(Math.random() * (1000 - 1 + 1) + 1);
 var QuantidadeChutes = 0;
+var MaximoErros = 15;
 
-while (Chute != NumeroSecreto)
+while (Chute != NumeroSecreto && QuantidadeChutes < MaximoErros)
 {
-    var Chute = prompt('Digite um número entre 1 e 1000');
+    var Chute = prompt('Digite um número entre 1 e 1000. Você tem 15 chutes');
     
     if (Chute == NumeroSecreto)
     {
@@ -19,13 +20,17 @@ while (Chute != NumeroSecreto)
     {
         if (Chute > NumeroSecreto)
         {
-            alert('Errado... O número secreto é menor do que ' + Chute)
-            QuantidadeChutes += 1
+            alert('Errado... O número secreto é menor do que ' + Chute + '. Ainda restam ' + (MaximoErros - 1 - QuantidadeChutes) + ' tentativas.')
         }
-        else if (Chute < NumeroSecreto)
+        if (Chute < NumeroSecreto)
         {
-            alert('Errou... O número secreto é maior do que ' + Chute)
-            QuantidadeChutes += 1
+            alert('Errou... O número secreto é maior do que ' + Chute + '. Ainda restam ' + (MaximoErros - 1 - QuantidadeChutes) + ' tentativas.')
         }
     }
+    QuantidadeChutes ++
+}
+
+if (QuantidadeChutes == MaximoErros)
+{
+    alert('Você atingiu o limite de chutes e perdeu :(. O número era ' + NumeroSecreto + '. Boa sorte na próxima!')
 }
