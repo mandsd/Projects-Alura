7718cbd0be70e6d707a8937bbf574c9e06217d76
mandsd <105010000+mandsd@users.noreply.github.com>
2023-01-31 17:23:28 -0300

    Update and rename Conversor_Ano-Luz_Km to Conversor_Ano-Luz_Km.js

---
+++ Conversor_Ano-Luz_Km.js
@@ -0,0 +1,23 @@
+// Abaixo está um projeto que converte ano-luz para Km(quilômetros).
+
+var cotacaoAnoLuz = 9.46;
+var valorEmKm;
+
+alert("Seja bem vindo!");
+nome = prompt("Qual o seu nome? ");
+valorEmAnoLuz = prompt(
+  "Olá, " + nome + ". Digite um valor em Ano-Luz para ser convertido em Km: "
+);
+
+valorEmKm = valorEmAnoLuz * cotacaoAnoLuz;
+valorEmKm = valorEmKm.toFixed(2);
+
+alert(
+  "O valor de " +
+    valorEmAnoLuz +
+    " ano-luz equivale à " +
+    valorEmKm +
+    " trilhões em Km"
+);
+
+// https://codepen.io/mandsdahm/full/poZxpma
